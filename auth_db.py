6f599e0ffcb0d31737521cc51e3f81a8b8196372
Your Name <you@example.com>
2025-07-19 14:59:14 +0530

    change requrent

--- auth_db.py
+++ auth_db.py
@@ -2,7 +2,6 @@ import mysql.connector
 
 conn = mysql.connector.connect(
     host = "sql12.freesqldatabase.com",
-    port = 3307,
     username = "sql12790788",
     password = "P9KQjG1LFF",
     database = "sql12790788"
